e45f829f682164aa7347e704ceb096ba886b4ed0
acalderon20 <acalderon20@coe.edu>
2023-10-26 10:19:12 -0500

    Initial Commit

---
+++ Stanford193p/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stanford193p
+//
+//  Created by Adolfo Calderon on 10/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stanford193p/Stanford193pApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stanford193pApp.swift
+//  Stanford193p
+//
+//  Created by Adolfo Calderon on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Stanford193pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
