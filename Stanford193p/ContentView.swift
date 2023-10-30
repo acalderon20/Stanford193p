604842f7d756a16edf9af74a8e84978a4fecab0e
acalderon20 <acalderon20@coe.edu>
2023-10-29 22:32:56 -0500

    Var: some View -> Encapsulates chunks of Views, helps readability when you use concise, descriptive names for the var: some Views

--- Stanford193p/ContentView.swift
+++ Stanford193p/ContentView.swift
@@ -9,21 +9,60 @@ import SwiftUI
 
 struct ContentView: View {
     let emojis : [String] = ["😂", "😭", "😈", "😼", "😳"]
+    @State var cardCount: Int = 4
     
     var body: some View {
+        VStack{
+            cards
+            cardCountAdjusters
+        }
+        .padding()
+    }
+    
+    var cardCountAdjusters: some View {
         HStack {
-            ForEach(emojis.indices, id : \.self) { index in
-                CardView(content: emojis[index], isFaceUp: true)
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    var cards: some View {
+        HStack {
+            ForEach(0..<cardCount, id : \.self) { index in
+                CardView(content: emojis[index])
             }
         }
         .foregroundColor(.orange)
         .padding()
     }
+    
+    var cardRemover: some View {
+        Button(action: {
+            if cardCount > 1 {
+                cardCount -= 1
+            }
+        }, label: {
+            Image(systemName: "rectangle.stack.fill.badge.minus")
+        })
+    }
+    var cardAdder: some View {
+        Button(action: {
+            if cardCount < emojis.count {
+                cardCount += 1
+            }
+        }, label: {
+            Image(systemName: "rectangle.stack.fill.badge.plus")
+        })
+    }
+    
+    
 }
 
 struct CardView: View {
     var content: String
-    @State var isFaceUp: Bool = false
+    @State var isFaceUp: Bool = true
     var body: some View {
         ZStack(content: {
             if isFaceUp {
