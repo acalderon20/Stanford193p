c8498bdc7929ba966f7200dfe193457d327a494d
acalderon20 <acalderon20@coe.edu>
2023-11-01 12:45:05 -0500

    Completed RT 8,9,10,11,12 Assignment 1 Complete!

--- Stanford193p/ContentView.swift
+++ Stanford193p/ContentView.swift
@@ -9,26 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var currentTheme: CardTheme = .halloween
+    @State var currentTheme: CardTheme = .cat
 //    @State var cardCount: Int = 4
     
     enum CardTheme {
-        case monkey, halloween, christmas
+        case sport, cat, christmas
     }
     
     var currentEmojis: [String] {
         switch currentTheme {
-        case .monkey:
-            return monkeyEmojis.shuffled()
-        case .halloween:
-            return halloweenEmojis.shuffled()
+        case .sport:
+            return sportEmojis.shuffled()
+        case .cat:
+            return catEmojis.shuffled()
         case .christmas:
             return christmasEmojis.shuffled()
         }
     }
 
-    let monkeyEmojis: [String] = ["🙈", "🙉", "🙊", "🐵", "🙈", "🙉", "🙊", "🐵"]
-    let halloweenEmojis: [String] = ["🎃", "👻", "🪦", "💀", "🎃", "👻", "🪦", "💀"]
+    let sportEmojis: [String] = ["🚴", "🏃", "🏋️", "🏌️", "🚴", "🏃", "🏋️", "🏌️"]
+    let catEmojis: [String] = ["😹", "😻", "🙀", "😿", "😹", "😻", "🙀", "😿"]
     let christmasEmojis: [String] = ["🎄", "🌲", "🎅", "🤶", "🎄", "🌲", "🎅", "🤶"]
     
     var body: some View {
@@ -41,8 +41,10 @@ struct ContentView: View {
         }
         .padding()
     }
+    
+    
     var title: some View {
-        Text("Memorize")
+        Text("Memorize!")
             .font(.largeTitle)
     }
     
@@ -50,7 +52,7 @@ struct ContentView: View {
         let shuffledCards = currentEmojis.shuffled()
         
         return LazyVGrid(columns: [GridItem(.adaptive(minimum: 145))]) {
-            ForEach(0..<monkeyEmojis.count, id : \.self) { index in
+            ForEach(0..<catEmojis.count, id : \.self) { index in
                 CardView(content: shuffledCards[index])
                     .aspectRatio(2/3, contentMode: .fit)
             }
@@ -60,29 +62,41 @@ struct ContentView: View {
     }
     
     var themes: some View {
-        VStack {
-            monkeyButton
-            halloweenButton
-            christmasButton
+        VStack(spacing: 5) {
+            sportTheme
+            catTheme
+            christmasTheme
         }
     }
     
-    func changeTheme(to theme: CardTheme, buttonName: String) -> some View {
+    func changeTheme(to theme: CardTheme, icon: String) -> some View {
         Button(action: {currentTheme = theme}) {
-            Text(buttonName)
+            Image(systemName: icon)
         }
     }
     
-    var monkeyButton: some View {
-        changeTheme(to: .monkey, buttonName: "Monkey")
+    var sportTheme: some View {
+        VStack {
+            Text("Sport Theme")
+            changeTheme(to: .sport, icon: "figure.run")
+        }
+        .font(.title2)
     }
     
-    var halloweenButton: some View {
-        changeTheme(to: .halloween, buttonName: "Halloween")
+    var catTheme: some View {
+        VStack {
+            Text("Cat Theme")
+            changeTheme(to: .cat, icon: "cat.fill")
+        }
+        .font(.title2)
     }
     
-    var christmasButton: some View {
-        changeTheme(to: .christmas, buttonName: "Christmas")
+    var christmasTheme: some View {
+        VStack {
+            Text("Christmas Theme")
+            changeTheme(to: .christmas, icon: "snowflake.circle.fill")
+        }
+        .font(.title2)
     }
 }
 
@@ -97,8 +111,8 @@ struct CardView: View {
                 base.strokeBorder(lineWidth: 2)
                 Text(content).font(.largeTitle) // emoji shown
             }
-            .opacity(isFaceUp ? 1 : 0)
-            base.fill().opacity(isFaceUp ? 0 : 1) // the "back" of the cards
+            .opacity(isFaceUp ? 0 : 1)
+            base.fill().opacity(isFaceUp ? 1 : 0)
         }
         .onTapGesture {
             isFaceUp.toggle()
